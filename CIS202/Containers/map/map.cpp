b7ce22f903e5d7eaace5fb83fddfde10742c4f0d
robertbarrera <129228925+robertbarrera@users.noreply.github.com>
2023-04-20 22:56:10 +0000

    updated file names

--- CIS202/Containers/map/map.cpp
+++ CIS202/Containers/map/map.cpp
@@ -1,3 +1,15 @@
+/*
+Given a map pre-filled with student names as keys and grades as values, complete main() by reading in the name of a student, outputting their original grade, and then reading in and outputting their new grade.
+
+Ex: If the input is:
+
+Quincy Wraight
+73.1
+
+the output is:
+Quincy Wraight's original grade: 65.4
+Quincy Wraight's new grade: 73.1
+*/
 #include <map>
 #include <string>
 #include <iostream>
